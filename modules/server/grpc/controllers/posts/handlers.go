7f4eec59f4035b19c802cfda81931ddf8398b916
Nikolai Varlamov <varlamow.col@yahoo.com>
2022-03-23 01:04:02 +0300

    implemented list method

--- modules/server/grpc/controllers/posts/handlers.go
+++ modules/server/grpc/controllers/posts/handlers.go
@@ -2,7 +2,7 @@ package posts
 
 import (
 	"context"
-	"fmt"
+	"errors"
 
 	"github.com/google/uuid"
 	"github.com/minish144/rentateam-test/gen/pb"
@@ -10,6 +10,7 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 func (c *GRPCControllerModule) Create(ctx context.Context, in *pb.Posts_CreateRequest) (*pb.Posts_CreateResponse, error) {
@@ -34,7 +35,6 @@ func (c *GRPCControllerModule) Create(ctx context.Context, in *pb.Posts_CreateRe
 			Tag:    tag,
 		})
 	}
-	fmt.Printf("%+v", in)
 
 	// creating new blog post and adding its tags in transaction
 	err := c.db.DB.Transaction(func(tx *gorm.DB) error {
@@ -80,5 +80,34 @@ func (c *GRPCControllerModule) Create(ctx context.Context, in *pb.Posts_CreateRe
 }
 
 func (c *GRPCControllerModule) List(ctx context.Context, in *pb.Posts_ListRequest) (*pb.Posts_ListResponse, error) {
-	return &pb.Posts_ListResponse{Posts: []*pb.Post{{Header: "test", Body: "test"}}}, nil
+	if err := in.Validate(); err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	var postsList []*pb.PostORM
+	result := c.db.DB.Preload(clause.Associations).Find(&postsList)
+	if result.Error != nil && !errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		c.log.WithFields(logrus.Fields{
+			"entity": "posts",
+			"method": "create",
+			"error":  result.Error.Error(),
+		}).Errorln("database error occured")
+		return nil, status.Error(codes.Internal, result.Error.Error())
+	}
+
+	var convertedList []*pb.Post
+	for _, ath := range postsList {
+		converted, err := ath.ToPB(ctx)
+		if err != nil {
+			c.log.WithFields(logrus.Fields{
+				"entity": "posts",
+				"method": "create",
+				"error":  err.Error(),
+			}).Errorln("could not convert orm structure to pb")
+			return nil, status.Error(codes.Internal, err.Error())
+		}
+		convertedList = append(convertedList, &converted)
+	}
+
+	return &pb.Posts_ListResponse{Posts: convertedList, Count: uint32(len(convertedList))}, nil
 }
